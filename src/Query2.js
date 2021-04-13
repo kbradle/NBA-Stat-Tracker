ce983ee2dcfb1242c99970af431abd120adae7ed
jakew54 <57453113+jakew54@users.noreply.github.com>
2021-04-13 01:22:42 -0400

    Update Query2.js

--- src/Query2.js
+++ src/Query2.js
@@ -17,7 +17,10 @@ function test(param) {
   var adjustedName2 = param.p2name.replaceAll("'", "''");
   var finalName2 = "UPPER('"+adjustedName2+"')";
 
-  var data = "query=SELECT Name, P36, gyear FROM (SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Extract(year from games.game_date) AS gyear FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName1 + ") AND games_details.seconds_played != 0 UNION SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Extract(year from games.game_date) AS gyear FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName2 + ") AND games_details.seconds_played != 0) ORDER BY gyear ASC";
+  //var data = "query=SELECT Name, P36, gyear FROM (SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Extract(year from games.game_date) AS gyear FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName1 + ") AND games_details.seconds_played != 0 UNION SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Extract(year from games.game_date) AS gyear FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName2 + ") AND games_details.seconds_played != 0) ORDER BY gyear ASC";
+
+  var data = "query=SELECT Name, P36, to_char(\"TRUNC(GAME_DATE,'MONTH')\",'YYYY-MM') as \"Date\" FROM (SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Trunc(Game_Date, 'Month') FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName1 + ") AND games_details.seconds_played != 0 UNION SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Trunc(Game_Date, 'Month') FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName2 + ") AND games_details.seconds_played != 0) ORDER BY \"Date\" ASC";
+
 
   //data = data.replaceAll("+", "%2B");
   xhr.send(data);
@@ -34,9 +37,19 @@ function test(param) {
 
       for (var i = 0; i < obj.message.rows.length; i++) {
         if (obj.message.rows[i][0].toLowerCase() === param.p1name.toLowerCase()) {
-          p1.push({ y: obj.message.rows[i][1], label: obj.message.rows[i][2] });
+          p1.push({ y: obj.message.rows[i][1], label: obj.message.rows[i][2] ,
+          x: new Date(
+            obj.message.rows[i][2].substring(0, 4),
+            obj.message.rows[i][2].substring(5, 7)
+          ),
+        });
         } else {
-          p2.push({ y: obj.message.rows[i][1], label: obj.message.rows[i][2] });
+          p2.push({ y: obj.message.rows[i][1], label: obj.message.rows[i][2] ,
+          x: new Date(
+            obj.message.rows[i][2].substring(0, 4),
+            obj.message.rows[i][2].substring(5, 7)
+          ),
+        });
         }
       }
     }
@@ -85,13 +98,14 @@ class Query2 extends React.Component {
     const options = {
       animationEnabled: true,
       title: {
-        text: "Players Per 36 Minutes",
+        text: "Stats Per 36-Minute for Players",
       },
       axisY: {
-          title: "" + this.state.chosenStat,
+          title: "" + this.state.chosenStat.replaceAll("_", " "),
       },
       axisX: {
-          title: "Date" //temp CHANGE
+        title: "Date",
+        valueFormatString: "MM-YYYY",
       },
       toolTip: {
         shared: true,
@@ -101,14 +115,12 @@ class Query2 extends React.Component {
           type: "spline",
           name: this.state.p1name,
           showInLegend: true,
-          markerType: "triangle",
           dataPoints: p1,
         },
         {
           type: "spline",
           name: this.state.p2name,
           showInLegend: true,
-          markerType: "square",
           dataPoints: p2,
         },
       ],
