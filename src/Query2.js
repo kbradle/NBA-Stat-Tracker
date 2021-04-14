a9e403263af227edbd2901b7d1d5fbf9a307ecde
jakew54 <57453113+jakew54@users.noreply.github.com>
2021-04-14 03:00:23 -0400

    Update Query2.js

--- src/Query2.js
+++ src/Query2.js
@@ -14,12 +14,29 @@ function test(param) {
 
   var adjustedName1 = param.p1name.replaceAll("'", "''");
   var finalName1 = "UPPER('"+adjustedName1+"')";
-  var adjustedName2 = param.p2name.replaceAll("'", "''");
-  var finalName2 = "UPPER('"+adjustedName2+"')";
 
-  //var data = "query=SELECT Name, P36, gyear FROM (SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Extract(year from games.game_date) AS gyear FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName1 + ") AND games_details.seconds_played != 0 UNION SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Extract(year from games.game_date) AS gyear FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName2 + ") AND games_details.seconds_played != 0) ORDER BY gyear ASC";
+  let monthOrYearString1;
+  let monthOrYearString2;
 
-  var data = "query=SELECT Name, P36, to_char(\"TRUNC(GAME_DATE,'MONTH')\",'YYYY-MM') as \"Date\" FROM (SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Trunc(Game_Date, 'Month') FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName1 + ") AND games_details.seconds_played != 0 UNION SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Trunc(Game_Date, 'Month') FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName2 + ") AND games_details.seconds_played != 0) ORDER BY \"Date\" ASC";
+  if(param.monthOrYear === "0") {
+    monthOrYearString1 = "to_char(\"TRUNC(GAME_DATE,'YEAR')\",'YYYY-MM')";
+    monthOrYearString2 = "Trunc(Game_Date, 'Year')";
+  } else {
+    monthOrYearString1 = "to_char(\"TRUNC(GAME_DATE,'MONTH')\",'YYYY-MM')";
+    monthOrYearString2 = "Trunc(Game_Date, 'Month')";
+  }
+
+  let data;
+
+  if (param.p2name === "Other" || param.p2name === "other") {
+    data = "query=SELECT Name, AVG(P36), " + monthOrYearString1 + " as \"Date\" FROM (SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, " + monthOrYearString2 + " FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName1 + ") AND games_details.seconds_played != 0 UNION SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, " + monthOrYearString2 + " FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) != " + finalName1 + ") AND games_details.seconds_played != 0) GROUP BY " + monthOrYearString1 + ", Name ORDER BY \"Date\" ASC";
+  } else {
+    var adjustedName2 = param.p2name.replaceAll("'", "''");
+    var finalName2 = "UPPER('"+adjustedName2+"')";
+    data = "query=SELECT Name, AVG(P36), " + monthOrYearString1 + " as \"Date\" FROM (SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, " + monthOrYearString2 + " FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName1 + ") AND games_details.seconds_played != 0 UNION SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, " + monthOrYearString2 + " FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName2 + ") AND games_details.seconds_played != 0) GROUP BY " + monthOrYearString1 + ", Name ORDER BY \"Date\" ASC";
+  }
+
+  //var data = "query=SELECT Name, P36, to_char(\"TRUNC(GAME_DATE,'MONTH')\",'YYYY-MM') as \"Date\" FROM (SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Trunc(Game_Date, 'Month') FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName1 + ") AND games_details.seconds_played != 0 UNION SELECT players.player_name AS Name, ((games_details." + param.chosenStat + " / games_details.seconds_played) * 2160) AS P36, Trunc(Game_Date, 'Month') FROM players, games_details, games WHERE games_details.player_ID = players.player_ID AND games.game_ID = games_details.game_ID AND (UPPER(players.player_name) = " + finalName2 + ") AND games_details.seconds_played != 0) ORDER BY \"Date\" ASC";
 
 
   //data = data.replaceAll("+", "%2B");
@@ -69,7 +86,7 @@ function test(param) {
 class Query2 extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { p1name: "", p2name: "", chosenStat: ""};
+    this.state = { p1name: "", p2name: "", chosenStat: "", monthOrYear: 0};
     this.updateChart = this.updateChart.bind(this);
     this.chart = "";
   }
@@ -85,6 +102,9 @@ class Query2 extends React.Component {
   };
   myChangeHandler3 = (event) => {
     this.setState({ chosenStat: event.target.value });
+  };
+  myChangeHandler4 = (event) => {
+    this.setState({ monthOrYear: event.target.value });
   };
   componentDidMount() {
     setInterval(this.updateChart);
@@ -156,6 +176,14 @@ class Query2 extends React.Component {
                   <option value={"turnovers"}>Turnovers</option>
                   <option value={"personal_fouls"}>Personal Fouls</option>
                   <option value={"points"}>Points</option>
+              </select>
+          </label>
+          <label>
+              Group By:
+              <select multiple={false} value={this.props.monthOrYear} onChange={this.myChangeHandler4}>
+                  <option value={""}>-- Choose Date Configuration --</option>
+                  <option value={"1"}>Month</option>
+                  <option value={"0"}>Year</option>
               </select>
           </label>
           <input type="submit" />
